8677cb0be9368b1d445e160cce42cafa843829e0
hkhalaf1675 <58273369+hkhalaf1675@users.noreply.github.com>
2024-04-27 13:11:19 -0700

    hash change profile picture

--- routes/api/users/usersRoutes.js
+++ routes/api/users/usersRoutes.js
@@ -50,27 +50,27 @@ router.delete('/delete/:id',
     usersMiddleware.remove
 );
 
-const destinationDir = path.join(__dirname, "../../../images");
-if(!fs.existsSync(destinationDir)){
-    fs.mkdirSync(destinationDir);
-}
+// const destinationDir = path.join(__dirname, "../../../images");
+// if(!fs.existsSync(destinationDir)){
+//     fs.mkdirSync(destinationDir);
+// }
 
-const storage = multer.diskStorage({
-    destination: function (req, file, callback){
-        callback(null, destinationDir);
-    },
+// const storage = multer.diskStorage({
+//     destination: function (req, file, callback){
+//         callback(null, destinationDir);
+//     },
 
-    filename: function(req, file, callback){
-        callback(null, new Date().toISOString().replace(/:/g, '_') + '_' + file.originalname);
-    }
-});
+//     filename: function(req, file, callback){
+//         callback(null, new Date().toISOString().replace(/:/g, '_') + '_' + file.originalname);
+//     }
+// });
 
-const upload = multer({ storage });
-router.put('/change-profile-picture/:id', 
-    userAuth,
-    validation({id: 'required|exist:User'}, true),
-    upload.single('profilePicture'),
-    usersMiddleware.changeProfilePicture
-);
+// const upload = multer({ storage });
+// router.put('/change-profile-picture/:id', 
+//     userAuth,
+//     validation({id: 'required|exist:User'}, true),
+//     upload.single('profilePicture'),
+//     usersMiddleware.changeProfilePicture
+// );
 
 module.exports = router;
